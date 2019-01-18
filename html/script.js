2f05eab859647e2ad7fe84e82950f69fca1c1bf6
penfold42 <penfold42@users.noreply.github.com>
2019-01-18 18:33:38 +1100

    WebUI: MQTT, cleanup, show set topic

--- html/script.js
+++ html/script.js
@@ -185,6 +185,10 @@ $(function() {
     });
 
     // MQTT field toggles
+    $('#mqtt_topic').keyup(function() {
+        updateMQTTSet();
+    });
+
     $('#mqtt').click(function() {
         if ($(this).is(':checked')) {
             $('.mqtt').removeClass('hidden');
@@ -292,6 +296,14 @@ $(function() {
     hash && $('ul.navbar-nav li a[href="' + hash + '"]').click();
 
 });
+
+function updateMQTTSet() {
+    if ( $('#mqtt_topic').val() ) {
+        $('#mqtt_topicset').val( $('#mqtt_topic').val() + '/set');
+    } else {
+        $('#mqtt_topicset').val( '' );
+    }
+}
 
 function wifiValidation() {
     var WifiSaveDisabled = false;
@@ -620,6 +632,7 @@ function getConfig(data) {
     $('#mqtt_user').val(config.mqtt.user);
     $('#mqtt_password').val(config.mqtt.password);
     $('#mqtt_topic').val(config.mqtt.topic);
+    updateMQTTSet();
     $('#mqtt_haprefix').val(config.mqtt.haprefix);
     $('#mqtt_clean').prop('checked', config.mqtt.clean);
     $('#mqtt_hadisco').prop('checked', config.mqtt.hadisco);
